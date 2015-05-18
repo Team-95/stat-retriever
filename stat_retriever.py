b7fcbc3a2117f00177ddd7a353eb6a4dee5bc777
Matthieu Grieger <matthieugrieger@gmail.com>
2015-05-17 23:51:26 -0700

    Fix formatting once more, added response parsing

--- stat_retriever.py
+++ stat_retriever.py
@@ -4,13 +4,19 @@ stat_retriever.py
 """
 
 import requests
+import json
 
 def main():
-    url = "http://stats.nba.com/stats/leaguedashplayerbiostats?College=&Conference="
-        "&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment="
-        "&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound="
-        "0&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&Season=2014-15&SeasonSegment="
-        "&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
+    season = "2014-15"
+    url = ("http://stats.nba.com/stats/leaguedashplayerbiostats?College=&Conference="
+    "&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment="
+    "&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound="
+    "0&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&Season=%s&SeasonSegment="
+    "&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference="
+    "&VsDivision=&Weight=") % season
+
+    response = requests.get(url)
+    stats = json.loads(response.text)
 
 if __name__ == "__main__":
     main()
