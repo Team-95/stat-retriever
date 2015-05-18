61dcdae285a3df2106eb97feb63841d0841e4222
Matthieu Grieger <matthieugrieger@gmail.com>
2015-05-17 23:45:28 -0700

    Add NBA stats URL

---
+++ stat_retriever.py
@@ -0,0 +1,16 @@
+"""
+stat-retriever by Team-95
+stat_retriever.py
+"""
+
+import requests
+
+def main():
+    url = "http://stats.nba.com/stats/leaguedashplayerbiostats?College=&Conference="
+    "&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment="
+    "&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound="
+    "0&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&Season=2014-15&SeasonSegment="
+    "&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
+
+if __name__ == "__main__":
+    main()
