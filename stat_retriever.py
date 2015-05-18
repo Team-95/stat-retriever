0dee4671d15a2bfb2e06516deaf349a7616f5c8c
Matthieu Grieger <matthieugrieger@gmail.com>
2015-05-17 23:46:15 -0700

    Formatting fix

--- stat_retriever.py
+++ stat_retriever.py
@@ -7,10 +7,10 @@ import requests
 
 def main():
     url = "http://stats.nba.com/stats/leaguedashplayerbiostats?College=&Conference="
-    "&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment="
-    "&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound="
-    "0&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&Season=2014-15&SeasonSegment="
-    "&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
+        "&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment="
+        "&Height=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&PORound="
+        "0&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&Season=2014-15&SeasonSegment="
+        "&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
 
 if __name__ == "__main__":
     main()
